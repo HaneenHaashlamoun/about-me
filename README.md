0b41f70db0eda4debfc5a0c58adf5aec5c386363
Haneen Haashlamoun <haneen.hashlamoun@gmail.com>
2021-07-06 17:26:18 +0300

    Add Lab03

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # About Me
 
-### a small quiz about the auther
+Hello my name is Haneen Hashlamoun im 32 years I have Computer Technology Diploma digree , two years experienced as developer.
+
+a small quiz about the auther
+and a small quiz guessing the right number. adding some loops and array.
 
 written by Haneen Hashlamoun 
+
+Rihan Foudeh helped with Naming the For loop.
+Farouq Ibrahim helped with the loops.
+
--- js/app.js
+++ js/app.js
@@ -5,16 +5,18 @@ let name = prompt('what is your name ?');
 alert('Welcome to this quiz ..!' + name);
 alert('Please Answer the following questions...');
 
+let score = 0;
 
 //MYNAME
 let myname = prompt('Is my name Haneen Y/N?');
 myname = myname.toUpperCase();
 if (myname == 'Y') {
-    console.log(myname);
+    //console.log(myname);
     alert('Correct!... Great you knew my name.😊');
+    score++;
 }
 else {
-    console.log(myname);
+    //console.log(myname);
     alert('Wrong answer ...! 🙁');
 }
 
@@ -22,11 +24,12 @@ else {
 let myage = prompt('am I 32 years Y/N ?');
 myage = myage.toUpperCase();
 if (myage == 'Y') {
-    console.log(myage);
+    //console.log(myage);
     alert('Correct!... Great you knew my age.😊');
+    score++;
 }
 else {
-    console.log(myage);
+    //console.log(myage);
     alert('Wrong answer ...! 🙁');
 }
 
@@ -34,11 +37,12 @@ else {
 let mymajor = prompt('Is my major related to computer Y/N ?');
 mymajor = mymajor.toLowerCase();
 if (mymajor == 'y') {
-    console.log(mymajor);
+    //console.log(mymajor);
     alert('Correct!... Great you knew my major.😊');
+    score++;
 }
 else {
-    console.log(myage);
+    //console.log(myage);
     alert('Wrong answer ...! 🙁');
 }
 
@@ -46,25 +50,84 @@ else {
 let myexp = prompt('I have two years of experience Y/N ?');
 myexp = myexp.toLowerCase();
 if (myexp == 'y') {
-    console.log(myexp);
+    //console.log(myexp);
     alert('Correct!... Great you knew my experience.😊');
+    score++;
 }
 else {
-    console.log(myexp);
+    //console.log(myexp);
     alert('Wrong answer ...! 🙁');
 }
 
-//MYKIDS
+// //MYKIDS
 let mykids = prompt('I have two Kids write Y/N ?');
 mykids = mykids.toLowerCase();
 if (mykids == 'y') {
-    console.log(mykids);
+    //console.log(mykids);
     alert('Correct!... Great you knew the number of my kids.😊');
+    score++;
 }
 else {
-    console.log(mykids);
+    //console.log(mykids);
     alert('Wrong answer ...! 🙁');
 }
 
-alert('thanks for answering the quiz ...! 😊😊😊')
+
+// Counter of the number
+
+for (let x = 0; x < 4; x++) {
+    let guessnum = prompt('Guess a number between 1-10 ?');
+    guessnum = parseInt(guessnum);
+
+    if (guessnum == 5) {
+        alert('Correct!... Great Job.😊');
+        score++;
+        break;
+    }
+    else if (guessnum > 5) {
+        alert('Try less 😊');
+    }
+    else if (guessnum < 5){
+        alert('Try more 😊');
+    }
+    else {
+        alert('Enter valid value');
+    }
+
+    //Number of attempts
+    if (x == 3) {
+        alert('Wrong answer the right answer is 5...! 🙁');
+    }
+
+}
+
+//the movie guess Array
+
+let movieArr = ['Forest Gump', 'matrix', 'Die Hard', 'Pinocchio', 'The Exorcist', 'Carol', 'Casablanca', 'TheIntern'];
+
+attemptsLoop: for (let x = 0; x < 6; x++) {
+    let movie = prompt('What is my favorite movie ??...');
+    for (let j = 0; j < 10; j++) {
+        if (movie == movieArr[j] ) {
+            alert('Great Job..! 😊');
+            // console.log(movieArr);
+            scores++;
+            break attemptsLoop;
+        }
+        else {
+            alert("ERROR");
+            break;
+        }
+    }
+    //Test Number of attempts
+    if (x == 5) {
+        alert('My Movies: (Forest Gump, matrix, Die Hard, Pinocchio, The Exorcist, Carol, Casablanca, TheIntern)');
+    }
+}
+
+alert(`DONE 😊, thanks for answering ${name} 😊😊😊. Your scored ${score} /7 `);
+
+
+
+
 
